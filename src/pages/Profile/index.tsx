22030e2926b2ce2785929ec79730d857ea19d256
Agustin <agustingonzaleztam@gmail.com>
2025-06-28 19:16:20 -0300

    Removed lines from profile

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,7 +1,4 @@
-//import { Lock } from 'lucide-react';
 import { useAuth } from '@/hooks/useAuth';
-//import Card from '@/components/ui/Card';
-//import Button from '@/components/ui/Button';
 import AdminProfile from './AdminProfile';
 
 const Profile = () => {
@@ -17,20 +14,7 @@ const Profile = () => {
 
   return (
     <div className="space-y-6 pb-16 md:pb-0">
-      {/* Profile Section */}
       <AdminProfile />
-      {/* Security Section */}
-      {/*<Card className="p-6">
-        <h2 className="text-xl font-semibold text-gray-900 mb-6">Seguridad</h2>
-        <div className="space-y-4">
-          <Button variant="outline" icon={<Lock size={18} />} fullWidth className="justify-start">
-            Cambiar contraseña
-          </Button>
-          <Button variant="danger" fullWidth className="justify-start">
-            Quiero eliminar mi cuenta
-          </Button>
-        </div>
-      </Card>*/}
     </div>
   );
 };
